a2d7d7d5af8c29432883296c2a0112fe67b04666
Vincent <rentalawrence@gmail.com>
2024-11-16 11:10:26 +0700

    update
    last update

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -17,30 +17,6 @@ class UsersTableSeeder extends Seeder
      // akun seeder ini cuman untuk local host.
     public function run()
     {
-        User::factory()->create([
-            'name'      => 'Admin',
-            'username'  => 'admin',
-            'email'     => 'admin@example.com',
-            'password'  => bcrypt('password'),
-            'type'      => User::ADMIN,
-        ]);
-
-        User::factory()->create([
-            'name'      => 'John Doe',
-            'username'  => 'johndoe',
-            'email'     => 'john@example.com',
-            'password'  => bcrypt('password'),
-            'type'      => User::DEFAULT,
-        ]);
-
-        User::factory()->create([
-            'name'      => 'Maya Doe',
-            'username'  => 'mayadoe',
-            'email'     => 'maya@example.com',
-            'password'  => bcrypt('password'),
-            'type'      => User::DEFAULT,
-        ]);
-
         User::factory()->count(10)->create();
     }
 }
